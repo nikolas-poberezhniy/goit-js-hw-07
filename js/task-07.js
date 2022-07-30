acc20e2be42392824bae4fbd75445ef66c57aa10
nikolas-poberezhniy <nikolas.poberezhniy@gmail.com>
2022-07-30 22:44:01 +0300

    й

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const range = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-
-range.addEventListener("input", (event) => {
-    text.style.fontSize = `${event.currentTarget.value}px`;
-})
-
